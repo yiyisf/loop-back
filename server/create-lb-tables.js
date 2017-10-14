0b0282da428c00990114eca4719ff7a4a50063c3
yiyi <yiyi119120@gmail.com>
2017-10-14 17:42:55 +0800

    Initial commit

---
+++ server/create-lb-tables.js
@@ -0,0 +1,11 @@
+/**
+ * Created by liuzhe on 2017/10/14.
+ */
+var server = require('./server');
+var ds = server.dataSources.db;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
